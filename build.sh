b9dfcfa3c1c068df54b0a429f6b4f1d0ba0687a3
misuti <h1710169@nushigh.edu.sg>
2020-07-13 11:59:22 +0800

    Added hotkeys Preparing to add central server

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 java -jar /Users/zy/Documents/GitHub/exam-clock-2020/build/libs/packr.jar /Users/zy/Documents/GitHub/exam-clock-2020/build/libs/packr-config-win.json
 java -jar /Users/zy/Documents/GitHub/exam-clock-2020/build/libs/packr.jar /Users/zy/Documents/GitHub/exam-clock-2020/build/libs/packr-config-mac.json
-zip -r /Users/zy/Documents/GitHub/exam-clock-2020/build/deploy/ExamClock-win.zip /Users/zy/Documents/GitHub/exam-clock-2020/build/deploy/win/
-zip -r /Users/zy/Documents/GitHub/exam-clock-2020/build/deploy/ExamClock-osx.zip /Users/zy/Documents/GitHub/exam-clock-2020/build/deploy/ExamClock.app/
+cd ./build/deploy/
+zip -vr exam-clock-win.zip win -x "*.DS_Store"
+zip -vr exam-clock-osx.zip ExamClock.app -x "*.DS_Store"
